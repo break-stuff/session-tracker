e4f31fbafcd1704003c9afd9c432d8fb90af23e1
Burton Smith <bsmith@amazazing.com>
2018-05-04 06:31:12 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <table class="table">
+        <thead class="thead-light">
+          <th class="sticky-top">Title</th>
+          <th class="sticky-top">Speaker</th>
+          <th class="sticky-top">Category</th>
+          <th class="sticky-top">Level</th>
+          <th class="sticky-top">Room</th>
+          <th class="sticky-top">Favorite</th>
+        </thead>
+        <tbody>
+          <template v-for="(sessionTime, index) in sessionTimes">
+            <tr class="bg-info" :key="index + 100">
+              <td>
+                <h4>{{ sessionTime }}</h4>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr v-for="session in getSessionsByTime(sessionTime)" :key="session.title">
+              <td>{{ session.title }}</td>
+              <td>{{ session.name }}</td>
+              <td>{{ session.primaryCategory }}</td>
+              <td>{{ difficulty(session.level) }}</td>
+              <td>{{ session.room }}</td>
+              <td>
+                <button @click="markFavorite(session)">
+                  <i v-if="session.favorite" class="fas fa-heart"></i>
+                  <i v-else class="far fa-heart"></i>
+                </button>
+              </td>
+            </tr>
+        </template>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { sessions } from "./sessions";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      sessions
+    };
+  },
+  methods: {
+    getSessionsByTime(time) {
+      return this.sessions.filter(x => x.time === time);
+    },
+    difficulty(level) {
+      switch (level) {
+        case "100":
+          return "Beginner";
+        case "200":
+          return "Intermediate";
+        case "300":
+          return "Advanced";
+        default:
+          return "Beginner";
+      }
+    },
+    markFavorite(session) {
+      session.favorite = !session.favorite;
+      localStorage.setItem("stirTrekSessions", JSON.stringify(this.sessions));
+    }
+  },
+  computed: {
+    sessionTimes() {
+      return [...new Set(this.sessions.map(x => x.time))];
+    },
+    categories() {
+      return [...new Set(this.sessions.map(x => x.primaryCategory))];
+    }
+  },
+  mounted() {
+    this.sessions =
+      JSON.parse(localStorage.getItem("stirTrekSessions")) || sessions;
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  border: 0;
+}
+
+table {
+  margin-bottom: 3rem;
 }
 </style>
---
+++ src/sessions.js
@@ -0,0 +1,469 @@
+export const sessions = [{
+    time: '7:30 AM',
+    room: 'Concourse',
+    name: 'Stir Trek',
+    primaryCategory: 'Other',
+    level: '100',
+    title: 'Breakfast',
+    favorite: false
+  },
+  {
+    time: '8:30 AM',
+    room: 'Blue',
+    name: 'Tim LeMaster',
+    primaryCategory: 'Data / Storage / BI',
+    level: '100',
+    title: 'Introduction to Convolutional Neural Networks',
+    favorite: false
+  },
+  {
+    time: '8:30 AM',
+    room: 'Gauntlet',
+    name: 'Michael Dowden',
+    primaryCategory: 'Architecture / Patterns',
+    level: '200',
+    title: 'Naming: The Art of Clarity',
+    favorite: false
+  },
+  {
+    time: '8:30 AM',
+    room: 'Green',
+    name: 'Craig Stuntz',
+    primaryCategory: 'Security',
+    level: '200',
+    title: 'Secure Applications, By Design',
+    favorite: false
+  },
+  {
+    time: '8:30 AM',
+    room: 'Orange',
+    name: 'Justin James',
+    primaryCategory: 'App Dev (Languages, Frameworks, etc.)',
+    level: '200',
+    title: 'Rapid REST API Development with Node and Sails',
+    favorite: false
+  },
+  {
+    time: '8:30 AM',
+    room: 'Purple',
+    name: 'David Neal',
+    primaryCategory: 'DevOps / ALM',
+    level: '100',
+    title: 'FSGD and the Art of Delivering Value',
+    favorite: false
+  },
+  {
+    time: '8:30 AM',
+    room: 'Red',
+    name: 'Chris DeMars',
+    primaryCategory: 'Design (UI, UX, IA, etc.)',
+    level: '100',
+    title: 'I Can\'t See: Low Vision A11y & Users',
+    favorite: false
+  },
+  {
+    time: '8:30 AM',
+    room: 'Thanos',
+    name: 'Steve Smith',
+    primaryCategory: 'Soft Skills',
+    level: '100',
+    title: 'Boom! From Combat Engineer to Software Engineer',
+    favorite: false
+  },
+  {
+    time: '8:30 AM',
+    room: 'Yellow',
+    name: 'Jeremy Clark',
+    primaryCategory: 'Architecture / Patterns',
+    level: '200',
+    title: 'DI Why? Getting a Grip on Dependency Injection',
+    favorite: false
+  },
+  {
+    time: '9:45 AM',
+    room: 'Blue',
+    name: 'Lars Klint',
+    primaryCategory: 'Iot / Maker',
+    level: '200',
+    title: 'Programming Robots ñ Making Friends with Cozmo',
+    favorite: false
+  },
+  {
+    time: '9:45 AM',
+    room: 'Gauntlet',
+    name: 'Jeff McKenzie',
+    primaryCategory: 'Testing / Quality',
+    level: '200',
+    title: 'Hey, You Got Your TDD in my SQL DB!',
+    favorite: false
+  },
+  {
+    time: '9:45 AM',
+    room: 'Green',
+    name: 'James Balmert',
+    primaryCategory: 'DevOps / ALM',
+    level: '200',
+    title: 'Always Be Delivering',
+    favorite: false
+  },
+  {
+    time: '9:45 AM',
+    room: 'Orange',
+    name: 'Kris Hatcher',
+    primaryCategory: 'Cloud / Infrastructure',
+    level: '100',
+    title: 'Climbing the (Elastic) Beanstalk',
+    favorite: false
+  },
+  {
+    time: '9:45 AM',
+    room: 'Purple',
+    name: 'Bobby Grayson',
+    primaryCategory: 'App Dev (Languages, Frameworks, etc.)',
+    level: '100',
+    title: 'Elixir From First Principles',
+    favorite: false
+  },
+  {
+    time: '9:45 AM',
+    room: 'Red',
+    name: 'Bill Sempf',
+    primaryCategory: 'Security',
+    level: '200',
+    title: 'What\'s New In Web Application Security?',
+    favorite: false
+  },
+  {
+    time: '9:45 AM',
+    room: 'Thanos',
+    name: 'Benjamin Bykowski',
+    primaryCategory: 'Web (HTML, JavaScript, CSS, etc.)',
+    level: '200',
+    title: 'Front End is the New Back End',
+    favorite: false
+  },
+  {
+    time: '9:45 AM',
+    room: 'Yellow',
+    name: 'Kevin Holtz',
+    primaryCategory: 'Soft Skills',
+    level: '200',
+    title: 'Square peg, round hole - Plugging development teams into business culture',
+    favorite: false
+  },
+  {
+    time: '11:00 AM',
+    room: 'Blue',
+    name: 'Rob Richardson',
+    primaryCategory: 'DevOps / ALM',
+    level: '200',
+    title: 'Kubernetes Test Drive',
+    favorite: false
+  },
+  {
+    time: '11:00 AM',
+    room: 'Gauntlet',
+    name: 'Drew Furgiuele',
+    primaryCategory: 'Data / Storage / BI',
+    level: '200',
+    title: 'Understanding SQL Server Execution Plans',
+    favorite: false
+  },
+  {
+    time: '11:00 AM',
+    room: 'Green',
+    name: 'Barry Tarlton',
+    primaryCategory: 'Iot / Maker',
+    level: '100',
+    title: 'The Pi, Python, & Paintball? Innovating with Affordable Tech!',
+    favorite: false
+  },
+  {
+    time: '11:00 AM',
+    room: 'Orange',
+    name: 'Mandar Malunjkar',
+    primaryCategory: 'Soft Skills',
+    level: '100',
+    title: 'Lean Communication',
+    favorite: false
+  },
+  {
+    time: '11:00 AM',
+    room: 'Purple',
+    name: 'Martine Dowden',
+    primaryCategory: 'Design (UI, UX, IA, etc.)',
+    level: '200',
+    title: 'Accessibility Cookbook: 10 easy recipes',
+    favorite: false
+  },
+  {
+    time: '11:00 AM',
+    room: 'Red',
+    name: 'Seth Petry-Johnson',
+    primaryCategory: 'Testing / Quality',
+    level: '200',
+    title: 'UI Tests Are Fun to Write (If You Write Them Right)',
+    favorite: false
+  },
+  {
+    time: '11:00 AM',
+    room: 'Thanos',
+    name: 'Marc Peabody',
+    primaryCategory: 'App Dev (Languages, Frameworks, etc.)',
+    level: '200',
+    title: 'Git Jungle Guided Tours',
+    favorite: false
+  },
+  {
+    time: '11:00 AM',
+    room: 'Yellow',
+    name: 'Brandon Bruno',
+    primaryCategory: 'Misc *.*',
+    level: '100',
+    title: 'Be Better at Email by Perfecting Your Outbox',
+    favorite: false
+  },
+  {
+    time: '12:00 PM',
+    room: 'Concourse',
+    name: 'Stir Trek',
+    primaryCategory: 'Other',
+    level: '100',
+    title: 'Lunch!',
+    favorite: false
+  },
+  {
+    time: '1:00 PM',
+    room: 'Blue',
+    name: 'Lars Klint',
+    primaryCategory: 'Soft Skills',
+    level: '200',
+    title: 'The Force Awakens - Mastering Your Inner Developer',
+    favorite: false
+  },
+  {
+    time: '1:00 PM',
+    room: 'Gauntlet',
+    name: 'Dave Wasmer',
+    primaryCategory: 'App Dev (Languages, Frameworks, etc.)',
+    level: '200',
+    title: 'Refactoring with Typescript',
+    favorite: false
+  },
+  {
+    time: '1:00 PM',
+    room: 'Green',
+    name: 'Adam Pasternack',
+    primaryCategory: 'Architecture / Patterns',
+    level: '100',
+    title: 'Angular + Ngrx:  how the Redux pattern helps you build scalable, maintainable apps',
+    favorite: false
+  },
+  {
+    time: '1:00 PM',
+    room: 'Orange',
+    name: 'Chad Green',
+    primaryCategory: 'Cloud / Infrastructure',
+    level: '100',
+    title: 'From Zero to Serverless',
+    favorite: false
+  },
+  {
+    time: '1:00 PM',
+    room: 'Purple',
+    name: 'Ronda Bergman',
+    primaryCategory: 'Misc *.*',
+    level: '100',
+    title: 'I\'d Hire more Women if they Would Apply!',
+    favorite: false
+  },
+  {
+    time: '1:00 PM',
+    room: 'Red',
+    name: 'Kevin Mack',
+    primaryCategory: 'Design (UI, UX, IA, etc.)',
+    level: '200',
+    title: 'UX in an API',
+    favorite: false
+  },
+  {
+    time: '1:00 PM',
+    room: 'Thanos',
+    name: 'Warner Moore',
+    primaryCategory: 'DevOps / ALM',
+    level: '200',
+    title: 'DevOops: SRE and Distributed Computing at Scale',
+    favorite: false
+  },
+  {
+    time: '1:00 PM',
+    room: 'Yellow',
+    name: 'Kim McGill',
+    primaryCategory: 'Soft Skills',
+    level: '100',
+    title: 'How to Make a Dysfunctional Team Function without Resorting to Death and Dismemberment',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Blue',
+    name: 'Dennis Dunn',
+    primaryCategory: 'App Dev (Languages, Frameworks, etc.)',
+    level: '200',
+    title: 'Design like Mythbusters',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Gauntlet',
+    name: 'Anna Heiermann',
+    primaryCategory: 'Testing / Quality',
+    level: '100',
+    title: 'QA is slowing my project down! Let\'s talk Risk Based Testing',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Green',
+    name: 'Richard Taylor',
+    primaryCategory: 'Testing / Quality',
+    level: '200',
+    title: 'Raising the Value of Your Unit Tests',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Orange',
+    name: 'Greg Malcolm',
+    primaryCategory: 'Web (HTML, JavaScript, CSS, etc.)',
+    level: '100',
+    title: 'Chrome Developer Tools: Raiding the Armory',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Purple',
+    name: 'Stephen Shary',
+    primaryCategory: 'Mobile / Client',
+    level: '200',
+    title: 'React Native: lickety-split',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Purple',
+    name: 'Marlena Bowen',
+    primaryCategory: 'Soft Skills',
+    level: '100',
+    title: 'How to Solve All Your Problems Without Really Knowing What You\'re Doing',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Red',
+    name: 'Shawn Price',
+    primaryCategory: 'Startups / Entrepreneurship',
+    level: '100',
+    title: 'Startups! The white knights of healthcare',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Thanos',
+    name: 'Brett Koenig',
+    primaryCategory: 'Misc *.*',
+    level: '100',
+    title: 'Blockchain: Worth more than a Bitcoin?',
+    favorite: false
+  },
+  {
+    time: '2:15 PM',
+    room: 'Yellow',
+    name: 'Ed Charbeneau',
+    primaryCategory: 'Architecture / Patterns',
+    level: '300',
+    title: 'Giving Clarity to LINQ Queries by Extending Expressions',
+    favorite: false
+  },
+  {
+    time: '3:30 PM',
+    room: 'Blue',
+    name: 'Angel Thomas',
+    primaryCategory: 'Soft Skills',
+    level: '100',
+    title: 'Deliver what the client wants instead of what they ask for and get them to ask for what they need!',
+    favorite: false
+  },
+  {
+    time: '3:30 PM',
+    room: 'Gauntlet',
+    name: 'Eric Potter',
+    primaryCategory: 'Misc *.*',
+    level: '100',
+    title: 'Code up an 8-bit Hero',
+    favorite: false
+  },
+  {
+    time: '3:30 PM',
+    room: 'Green',
+    name: 'Jarred Olson',
+    primaryCategory: 'Cloud / Infrastructure',
+    level: '100',
+    title: 'Build your own Voice Assistant',
+    favorite: false
+  },
+  {
+    time: '3:30 PM',
+    room: 'Orange',
+    name: 'Jon Kruger',
+    primaryCategory: 'Misc *.*',
+    level: '100',
+    title: 'Ditching The Office',
+    favorite: false
+  },
+  {
+    time: '3:30 PM',
+    room: 'Purple',
+    name: 'Tim Hoolihan',
+    primaryCategory: 'Data / Storage / BI',
+    level: '200',
+    title: 'Machine Learning in R',
+    favorite: false
+  },
+  {
+    time: '3:30 PM',
+    room: 'Red',
+    name: 'Thomas Haver',
+    primaryCategory: 'Testing / Quality',
+    level: '100',
+    title: 'Microsoft Edge Test Automation: painful like Internet Explorer, but not in the same way',
+    favorite: false
+  },
+  {
+    time: '3:30 PM',
+    room: 'Thanos',
+    name: 'Wasim Hanna',
+    primaryCategory: 'App Dev (Languages, Frameworks, etc.)',
+    level: '200',
+    title: 'BFFs, Angular & .NET Core',
+    favorite: false
+  },
+  {
+    time: '3:30 PM',
+    room: 'Yellow',
+    name: 'Johnson Denen',
+    primaryCategory: 'DevOps / ALM',
+    level: '200',
+    title: 'Contain Yourself: Docker for the Entire SDLC',
+    favorite: false
+  },
+  {
+    time: '5:30 PM',
+    room: 'All Theaters',
+    name: 'Stir Trek',
+    primaryCategory: 'Other',
+    level: '100',
+    title: 'Movie Screening: Avengers Infinity War',
+    favorite: false
+  }
+]
