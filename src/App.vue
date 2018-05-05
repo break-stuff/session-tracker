ef84fb46851300a50557171a4277c730c7b14957
BWSmith <BWsmith@BSmith-MAC.local>
2018-05-05 08:32:00 -0400

    added filtering

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+      <h5>Filters:</h5>
+      <filter-controls :filter.sync="filters" :times="sessionTimes" :categories="categories" :rooms="rooms" :levels="levels"></filter-controls>
+      <br>
+      <br>
       <table class="table">
         <thead class="thead-light">
           <th class="sticky-top">Title</th>
@@ -25,7 +29,7 @@
               <td>{{ session.title }}</td>
               <td>{{ session.name }}</td>
               <td>{{ session.primaryCategory }}</td>
-              <td>{{ difficulty(session.level) }}</td>
+              <td>{{ getExperienceLevel(session.level) }}</td>
               <td>{{ session.room }}</td>
               <td>
                 <button @click="markFavorite(session)">
@@ -41,36 +45,79 @@
 </template>
 
 <script>
-import { sessions } from "./sessions";
+import { sessions } from "./data/sessions";
+import FilterControls from "./components/FilterControls";
 
 export default {
   name: "app",
   components: {
+    "filter-controls": FilterControls
   },
   data() {
     return {
-      sessions
+      sessions,
+      filteredSessions : [],
+      filters: {
+        time: null,
+        categories: null,
+        level: null,
+        room: null,
+        favorites: false
+      },
+      levels : [
+        {
+          id: '100',
+          name: 'Beginner'
+        },
+        {
+          id: '200',
+          name: 'Intermediate'
+        },
+        {
+          id: '300',
+          name: 'Advanced'
+        }
+      ]
     };
   },
   methods: {
     getSessionsByTime(time) {
-      return this.sessions.filter(x => x.time === time);
+      return this.filteredSessions.filter(x => x.time === time);
     },
-    difficulty(level) {
-      switch (level) {
-        case "100":
-          return "Beginner";
-        case "200":
-          return "Intermediate";
-        case "300":
-          return "Advanced";
-        default:
-          return "Beginner";
-      }
+    getExperienceLevel(level) {
+      return this.levels.find(x=>x.id === level).name;
     },
     markFavorite(session) {
-      session.favorite = !session.favorite;
-      localStorage.setItem("stirTrekSessions", JSON.stringify(this.sessions));
+      let sessions = this.getSessions();
+      sessions.find(x => x.title === session.title).favorite = !session.favorite;
+      this.saveSessions(sessions);
+      this.filteredSessions = this.getFilterSessions(this.filters);
+    },
+    saveSessions(sessions) {
+      localStorage.setItem("session-tracker", JSON.stringify(sessions));
+    },
+    getSessions() {
+      return JSON.parse(localStorage.getItem("session-tracker")) || sessions;
+    },
+    getFilterSessions(filters) {
+        let sessions = this.getSessions();
+
+        if (filters.time)
+          sessions = sessions.filter(x => x.time === filters.time);
+
+        if (filters.categories)
+          sessions = sessions.filter(x => x.primaryCategory === filters.categories);
+
+        if (filters.level)
+          sessions = sessions.filter(x => x.level === filters.level);
+
+        if (filters.rooms)
+          sessions = sessions.filter(x => x.room === filters.rooms);
+
+        if (filters.favorites)
+          sessions = sessions.filter(x => x.favorite === filters.favorites);
+
+        return sessions;
     }
   },
   computed: {
@@ -78,12 +125,22 @@ export default {
       return [...new Set(this.sessions.map(x => x.time))];
     },
     categories() {
-      return [...new Set(this.sessions.map(x => x.primaryCategory))];
+      return [...new Set(this.sessions.map(x => x.primaryCategory).sort())];
+    },
+    rooms() {
+      return [...new Set(this.sessions.map(x => x.room))];
     }
   },
   mounted() {
-    this.sessions =
-      JSON.parse(localStorage.getItem("stirTrekSessions")) || sessions;
+    this.filteredSessions = this.getSessions();
+  },
+  watch: {
+    filters: {
+      handler(newFilter) {
+        this.filteredSessions = this.getFilterSessions(newFilter);
+      },
+      deep: true
+    }
   }
 };
 </script>
